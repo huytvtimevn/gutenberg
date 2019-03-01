5ca4a7be93e35ec83833fa3c403294cf76ca2bbf
Andrew Duthie <andrew@andrewduthie.com>
2019-03-01 17:07:54 -0500

    Testing: Disable CSS animations without style dependency

--- packages/e2e-tests/plugins/disable-animations.php
+++ packages/e2e-tests/plugins/disable-animations.php
@@ -11,8 +11,10 @@
  * Enqueue CSS stylesheet disabling animations.
  */
 function enqueue_disable_animations_stylesheet() {
+	wp_register_style( 'gutenberg-disable-animations', null, array() );
+	wp_enqueue_style( 'gutenberg-disable-animations' );
 	$custom_css = '* { animation-duration: 0ms !important; }';
-	wp_add_inline_style( 'wp-components', $custom_css );
+	wp_add_inline_style( 'gutenberg-disable-animations', $custom_css );
 }
 
 add_action( 'enqueue_block_editor_assets', 'enqueue_disable_animations_stylesheet' );
