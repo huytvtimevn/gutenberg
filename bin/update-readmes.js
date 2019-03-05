b8377123738cf0cdf9c54ec2984119f2bb353ac0
Andrés <andres.maneiro@automattic.com>
2019-03-05 02:49:58 +0100

    Use new script

--- bin/update-readmes.js
+++ bin/update-readmes.js
@@ -5,15 +5,31 @@ const childProcess = require( 'child_process' );
 
 const packages = [
 	'e2e-test-utils',
+	'rich-text',
 ];
 
-let aggregatedExitCode = 0;
-packages.forEach( ( packageName ) => {
-	const args = [
+const getArgsForPackage = ( packageName ) => {
+	const defaultArgs = [
 		`packages/${ packageName }/src/index.js`,
 		`--output packages/${ packageName }/README.md`,
 		'--to-token',
 	];
+
+	const argsForPackage = {
+		'rich-text': [
+			`packages/${ packageName }/src/index.js`,
+			`--output packages/${ packageName }/README.md`,
+			'--to-token',
+			'--ignore "^unstable|^apply$|^changeListType$"',
+		],
+	};
+
+	return argsForPackage[ packageName ] || defaultArgs;
+};
+
+let aggregatedExitCode = 0;
+packages.forEach( ( packageName ) => {
+	const args = getArgsForPackage( packageName );
 	const pathToDocGen = path.join( __dirname, '..', 'node_modules', '.bin', 'docgen' );
 	const { status, stderr } = childProcess.spawnSync(
 		pathToDocGen,
